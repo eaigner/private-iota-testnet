7083546a041d01474e88efef41ebf52859d4606f
Erik Aigner <aigner.erik@gmail.com>
2018-01-06 21:06:20 +0100

    Build iri with generated snapshot

---
+++ build_iri_with_snapshot.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ ! -d "iri" ]; then
+	git clone https://github.com/iotaledger/iri
+fi
+
+pushd iri
+
+git reset --hard tags/v1.4.1.6
+git clean -fdx
+git apply ../iri-snapshot.patch
+
+cp ../Snapshot.txt src/main/resources
+
+mvn package -Dmaven.test.skip=true
+
+cp target/iri-*.jar ../iri-testnet.jar
