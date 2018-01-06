9002e45b5b419bc7eda66d6e8e699e0d77d80b28
Erik Aigner <aigner.erik@gmail.com>
2018-01-07 00:48:46 +0100

    Revert "Reuse IRI from snapshot build"
    This reverts commit dbf079337a63fcaf74c9970da76fd817d4b54404.

--- build_testnet_wallet.sh
+++ build_testnet_wallet.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -o xtrace
-
 rm -rf wallet
 
 git clone https://github.com/iotaledger/wallet
@@ -9,12 +7,18 @@ git clone https://github.com/iotaledger/wallet
 pushd wallet
 
 git clean -fdx
-git checkout tags/v2.5.1
+git reset --hard tags/v2.5.1
 
-mkdir -p iri
+git clone https://github.com/iotaledger/iri
 
-cp ../iri-testnet.jar iri/
-mv package.testnet.json package.json
+pushd iri
+
+mvn package
+cp target/iri-*.jar iri-testnet.jar
+
+popd
+
+cp package.testnet.json package.json
 
 brew update
 brew install node@8
@@ -22,11 +26,10 @@ brew link node@8
 
 export PATH="/usr/local/opt/node@8/bin:$PATH"
 
-node -v
-npm -v
-
 npm update
+npm install bower
 npm install
+
 npm run compile:mac
 
-mv out/mac/*.app ..
+mv out/mac/*.app .
